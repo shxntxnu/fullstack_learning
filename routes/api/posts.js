73364afd1012b5fd981b9ff2feb5476176f6f38d
shxntxnu <shantanuthebrave@gmail.com>
2026-08-11 13:20:57 +0100

    setup default routes for api endpoints

---
+++ routes/api/posts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/posts
+// @desc    Test route
+// @access  Public (don't need token if public)
+router.get('/', (req, res) => res.send('Posts route'));
+
+module.exports = router;
