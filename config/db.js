58f38e85a7140e9e496861c462941f9088e5f203
shxntxnu <shantanuthebrave@gmail.com>
2026-08-11 13:07:21 +0100

    initialised database

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');  // get any of values in json format
+
+// need something to call within server.js
+const connectDB = async() => {
+    try {
+        await mongoose.connect(db);  // returns a promise. Need await here
+        console.log("MongoDB Connected...");  // once connected, log it
+    }
+    catch (err) {
+        console.error(err.message);
+        process.exit(1);  // exit process with failure
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect to database using function
+connectDB();
 
 app.get('/', (req, res) => res.send("API Running"));
 
