3db2f9b7431756bd7b116bc6a1a4e47b983904fe
shxntxnu <shantanuthebrave@gmail.com>
2026-08-13 14:16:35 +0100

    get all profiles and get user by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -108,4 +108,41 @@ router.post('/', [auth, [
 
 });
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+    try {
+        // get all profiles
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.send(500).send('Server Error');
+    }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        // get all profiles
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+        // check if
+        if (!profile) {
+            res.status(400).json({ msg: 'Profile not found' });
+        }
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
