9feccb35a4cf97bea4265cf0cea3f9a12f611e5e
shxntxnu <shantanuthebrave@gmail.com>
2026-08-13 14:40:48 +0100

    delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -145,4 +145,25 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user, and posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+    try {
+        // remove user's posts
+
+        // remove profile
+        await Profile.findOneAndDelete({ user: req.user.id });
+        // remove user
+        await User.findOneAndDelete({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.send(500).send('Server Error');
+    }
+});
+
 module.exports = router;
