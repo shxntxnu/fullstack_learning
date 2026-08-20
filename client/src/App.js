3a078f6f720123e9f18a9c08c65a9a0017ac9e99
shxntxnu <shantanuthebrave@gmail.com>
2026-08-20 11:48:40 +0100

    initialised frontend with login and register links

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import './App.css';
+
+const App = () =>
+  // Fragment is a ghost element, won't show up in DOM
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Fragment>
+  </Router>
+
+export default App;
