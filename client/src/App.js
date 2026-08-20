b861fc386df0a617565cfa4010a55969316b7798
shxntxnu <shantanuthebrave@gmail.com>
2026-08-20 13:13:29 +0100

    setup redux store to enable use of action reducers

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,25 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () =>
-  // Fragment is a ghost element, won't show up in DOM
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+  {/* // Fragment is a ghost element, won't show up in DOM */}
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
 
 export default App;
