7771fd4ced83547e6f5ff43091b49a7019425865
shxntxnu <shantanuthebrave@gmail.com>
2026-08-20 13:01:39 +0100

    implemented login and register features that also connect to database

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,74 @@
-import React from 'react';
+import { Fragment, useState } from "react";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    // destructuring
+    const { email, password } = formData;
+
+    const onChange = e =>
+        setFormData({ ...formData, [e.target.name]: e.target.value }); // spread operator ... used to copy data directly
+
+    const onSubmit = async e => {
+        e.preventDefault();
+            console.log('SUCCESS!');
+            
+            // const newUser = {
+            //     name, 
+            //     email,
+            //     password
+            // }
+
+            // try {
+            //     const config = {
+            //         headers: {
+            //             'Content-Type': 'application/json'
+            //         }
+            //     }
+
+            //     const body = JSON.stringify(newUser);
+            //     const res = await axios.post('/api/users', body, config); // to hit the api/users endpoint
+
+            //     console.log(res.data);
+            // } catch (err) {
+            //     console.error(err.response.data);
+            // }
+    }
+
+    return <Fragment>
+        <section className="container">
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Sign Into Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="email"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email} onChange={(e) => onChange(e)} required />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => onChange(e)}
+                        minLength="6"
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to ="/register">Sign Up</Link>
+            </p>
+        </section>
+    </Fragment>;
 };
 
 export default Login;
