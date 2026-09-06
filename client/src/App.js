dc9379997463cfdec258cdc2c100f1c6830adf23
shxntxnu <shantanuthebrave@gmail.com>
2026-09-06 18:13:43 +0100

    added features for browsing all profiles/developers

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -43,6 +44,7 @@ const App = () =>{
                 <Routes>
                   <Route path="register" element={<Register />} />
                   <Route path="login" element={<Login />} />
+                  <Route path="profiles" element={<Profiles />} />
                   <Route path="dashboard" element={<PrivateRoute component={Dashboard} />} />
                   <Route path="create-profile" element={<PrivateRoute component={CreateProfile} />} />
                   <Route path="edit-profile" element={<PrivateRoute component={EditProfile} />} />
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,9 +3,11 @@ import { setAlert } from './alert';
 import {
     CLEAR_PROFILE,
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
     UPDATE_PROFILE,
-    ACCOUNT_DELETED
+    ACCOUNT_DELETED,
+    GET_REPOS
 } from './types';
 
 // Get surrent user's profile
@@ -15,6 +17,61 @@ export const getCurrentProfile = () => async dispatch => {
 
         dispatch({
             type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get('/api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get profile by ID
+export const getProfileById = id => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get(`/api/profile/user/${id}`);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
             payload: res.data
         });
     } catch (err) {
