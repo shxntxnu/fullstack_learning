3b3b5068d71e630c384b3fd52e6e5484d9bf51f2
shxntxnu <shantanuthebrave@gmail.com>
2026-09-06 13:13:42 +0100

    feature to delete education, experience, and entire account (including posts)

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,8 @@
+// Alerts
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+// Auth
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
@@ -7,7 +10,10 @@ export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
+
+// Profile actions
 export const GET_PROFILE = 'GET_PROFILE';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -153,7 +154,7 @@ router.get('/user/:user_id', async (req, res) => {
 router.delete('/', auth, async (req, res) => {
     try {
         // remove user's posts
-
+        await Post.deleteMany({ user: req.user.id });
         // remove profile
         await Profile.findOneAndDelete({ user: req.user.id });
         // remove user
